96a3fe8c4595ade133615db5e80134c30b762a8f
Markus <markus.kuntner@tirol.gv.at>
2020-04-09 22:03:44 +0200

    Version RC1

--- src/Program.cs
+++ src/Program.cs
@@ -63,7 +63,7 @@ namespace GRAMM_2001
             Console.WriteLine("");
             Console.WriteLine("+------------------------------------------------------+");
             Console.WriteLine("|                                                      |");
-            string Info =     "+         > > G R A M M VERSION: 21.01Beta1 < <        +";
+            string Info =     "+         > > G R A M M VERSION: 20.06RC1 < <          +";
             Console.WriteLine(Info);
             if (unix)
             {
